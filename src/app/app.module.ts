d3693b61bee25bf788d71e3eff1320aaef2362dd
Jerolivine <asentunali@gmail.com>
2019-12-29 21:11:29 +0300

    created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { AsyncSubjectComponent } from './components/async-subject/async-subject.component';
-import { BehaviorSubjectComponent } from './components/behavior-subject/behavior-subject.component';
-import { ReplaySubjectComponent } from './components/replay-subject/replay-subject.component';
-import { SubjectComponent } from './components/subject/subject.component';
+import { AppComponent } from "./app.component";
+import { AsyncSubjectComponent } from "./components/async-subject/async-subject.component";
+import { BehaviorSubjectComponent } from "./components/behavior-subject/behavior-subject.component";
+import { ReplaySubjectComponent } from "./components/replay-subject/replay-subject.component";
+import { SubjectComponent } from "./components/subject/subject.component";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, AsyncSubjectComponent, BehaviorSubjectComponent, ReplaySubjectComponent, SubjectComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    AsyncSubjectComponent,
+    BehaviorSubjectComponent,
+    ReplaySubjectComponent,
+    SubjectComponent
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/subject/subject.component.ts
+++ src/app/components/subject/subject.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Subject } from "rxjs";
 
 @Component({
-  selector: 'app-subject',
-  templateUrl: './subject.component.html',
-  styleUrls: ['./subject.component.css']
+  selector: "app-subject",
+  templateUrl: "./subject.component.html",
+  styleUrls: ["./subject.component.css"]
 })
 export class SubjectComponent implements OnInit {
+  public header = "Subject";
+  public messages: string[] = [];
+  subject = new Subject();
 
-  constructor() { }
+  ngOnInit(): void {
+    this.subject.next(1);
+    this.subject.next(2);
+    this.subject.next(3);
+    this.subject.next(4);
+    this.subject.subscribe(data => {
+      this.messages.push("Subscriber A: " + data);
+      console.log("Subscriber A:", data);
+    });
 
-  ngOnInit() {
+    this.subject.next(5);
+    this.subject.next(6);
+
+    // subscriber 2
+    this.subject.subscribe(data => {
+      this.messages.push("Subscriber B: " + data);
+      console.log("Subscriber B:", data);
+    });
+
+    this.subject.next(7);
+    this.subject.complete();
+    this.subject.next(8);
   }
 
+  ngOnDestroy(): void {
+    this.subject.unsubscribe();
+  }
 }
